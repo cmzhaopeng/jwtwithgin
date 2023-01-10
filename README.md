bc42f96ebcdaac641ca67723ec26b71aafd110d6
unknown <cmzhaopeng@139.com>
2023-01-11 00:11:02 +0800

    md the sub folder and the service package

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 
+
+# JWT Authentication in Golang with Gin
+reference:
+https://articles.wesionary.team/jwt-authentication-in-golang-with-gin-63dbc0816d55
---
+++ src/service/JWTAuthentication.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type JWTServicer interface {
+	GenerateToken(email string, isUser bool) string
+	ValidateToken(token string) (*jwt.Token, error)
+}
+
+type authCustomClaims struct {
+	Name string `json:"name"`
+	User bool   `json:"user"`
+	jwt.StandardClaims
+}
+
+type jwtService struct {
+	secretKey string
+	issuer    string
+}
+
+func (service *jwtService) GenerateToken(email string, isUser bool) string {
+	claims := &authCustomClaims{
+		email,
+		isUser,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+			Issuer:    service.issuer,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString([]byte(service.secretKey))
+	if err != nil {
+		panic(err)
+	}
+	return t
+	//signedToken, _ := token.SignedString([]byte(service.secretKey))
+	//return signedToken
+}
+
+func (service *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	return jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		if _, isvalid := token.Method.(*jwt.SigningMethodHMAC); !isvalid {
+			return nil, fmt.Errorf("Invalid token", token.Header["alg"])
+		}
+
+		return []byte(service.secretKey), nil
+	})
+}
