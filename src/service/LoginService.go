6bd7f8b155e4684b3f52479cb1e10e946837af50
鹏 赵 <zhaopeng@cmdi.chinamobile.com>
2023-01-17 12:23:41 +0800

    modify LoginService.go, get .env APP_KEY as password verify the nodejs frontend that will get the jwt and api

--- src/service/LoginService.go
+++ src/service/LoginService.go
@@ -1,5 +1,12 @@
 package service
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type LoginService interface {
 	LoginUser(email string, password string) bool
 }
@@ -10,12 +17,22 @@ type loginInformation struct {
 }
 
 func StaticLoginService() LoginService {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return &loginInformation{
+			email:    "testing@139.com",
+			password: "LiriTesJ",
+		}
+	}
 	return &loginInformation{
-		email:    "testing@139.com",
-		password: "LiriTesJ",
+		email:    "",
+		password: os.Getenv("APP_KEY"),
 	}
 }
 
 func (info *loginInformation) LoginUser(email string, password string) bool {
-	return info.email == email && info.password == password
+	//return info.email == email && info.password == password
+	//verify the app key. This is a static key. We should verify the user stored in the database on the future.
+	return info.password == password
 }
